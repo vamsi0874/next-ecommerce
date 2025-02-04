1c42f8d7989c2eaffdc0b340260b022206527fda
vamsi0874 <vamsim0874>
2025-02-04 10:06:23 +0530

    update

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -9,7 +9,7 @@ const CategoryList = async () => {
 
   return (
     <div className="px-4 overflow-x-scroll scrollbar-hide">
-      <div className="flex gap-4 md:gap-8">
+      <div className="flex flex-wrap justify-center gap-4 md:gap-8">
         {cats.items.map((item) => (
           <Link
             href={`/list?cat=${item.slug}`}
